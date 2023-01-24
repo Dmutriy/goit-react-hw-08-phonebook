27d0743c190408821583ddaeb3a92d9cebf6078f
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-01-24 17:57:33 +0200

    Create operations.js

---
+++ src/redux/operations.js
@@ -0,0 +1,56 @@
+// https://63cda2c7fba6420d4d72803f.mockapi.io/
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+//  import { getContacts } from './selectors';
+
+axios.defaults.baseURL = 'https://63cda2c7fba6420d4d72803f.mockapi.io';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/contacts');
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (text, thunkAPI) => {
+    try {
+      const response = await axios.post('/contacts', { text });
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (contactId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactId}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+//  export const toggleCompleted = createAsyncThunk(
+//    'tasks/toggleCompleted',
+//    async (task, thunkAPI) => {
+//      try {
+//        const response = await axios.put(`/tasks/${task.id}`, {
+//          completed: !task.completed,
+//        });
+//        return response.data;
+//      } catch (e) {
+//        return thunkAPI.rejectWithValue(e.message);
+//      }
+//    }
+//  );
