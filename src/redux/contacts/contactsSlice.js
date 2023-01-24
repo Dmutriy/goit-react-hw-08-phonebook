246a81b2100c880d29a9c0860cde10286023a2d4
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-01-24 17:57:39 +0200

    Update contactsSlice.js

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -1,7 +1,24 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit';
+import {
+  fetchContacts,
+  // addTask,
+  // deleteTask,
+  // toggleCompleted,
+} from '../operations';
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
 
 const contactsInitialState = {
   items: [],
+  // isLoading: false,
+  // error: null,
 };
 
 const contactsSlice = createSlice({
@@ -26,6 +43,15 @@ const contactsSlice = createSlice({
         contact => contact.id !== action.payload
       );
     },
+  },
+  extraReducers: {
+    [fetchContacts.pending]: handlePending,
+    [fetchContacts.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = action.payload;
+    },
+    [fetchContacts.rejected]: handleRejected,
   },
 });
 
