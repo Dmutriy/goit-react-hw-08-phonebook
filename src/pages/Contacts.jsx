db22d3e47ca4bb1f429cbd8158213e50a105e35a
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-02-02 22:27:30 +0200

    Create Contacts.jsx

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,39 @@
+import Section from '../components/Section';
+import Filter from '../components/Filter';
+import ContactsList from '../components/ContactList';
+import ContactForm from '../components/ContactForm';
+import { Wrap } from '../components/App.styled';
+
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  getError,
+  getIsLoading,
+  getContacts,
+} from '../redux/contacts/selectors';
+
+import { getFilter } from '../redux/filters/selectors';
+import { fetchContacts } from 'redux/contacts/operations';
+
+export default function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <Wrap>
+      <ContactForm />
+      <Section>
+        <Filter />
+        {isLoading && !error && <b>Loading...</b>}
+        {contacts.length > 0 && <ContactsList contacts={filter} />}
+      </Section>
+    </Wrap>
+  );
+}
