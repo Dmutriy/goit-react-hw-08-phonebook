e0edf9c25d9ad984b20adf2868263efc86e70db2
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-02-02 22:26:49 +0200

    Create AppBar.styled.jsx

---
+++ src/components/AppBar/AppBar.styled.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  border-bottom: 2px solid #2a363b;
+  width: 100%;
+  background-color: #1173ab;
+  background-color: transparent;
+  background-size: 200px 140px;
+  background-repeat: no-repeat;
+  background-position: center;
+  color: aqua;
+`;
+
+export const Container = styled.div`
+  max-width: 1600px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+  margin: 0 auto;
+  padding: 50px 20px;
+`;
