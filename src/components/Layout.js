39cf30393b23eead506deee5be8ff71798b974ac
Dmutriy <105307970+Dmutriy@users.noreply.github.com>
2023-02-03 20:26:36 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,18 +2,18 @@ import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AppBar } from './AppBar/AppBar';
 import { Suspense } from 'react';
-import ParticlesBackground from './Particles/ParticlesBackground';
+// import ParticlesBackground from './Particles/ParticlesBackground';
 
 export const Layout = () => {
   return (
     <>
       <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
-        <ParticlesBackground />
         <AppBar />
         <Suspense fallback={null}>
           <Outlet />
         </Suspense>
         <Toaster position="top-right" reverseOrder={false} />
+        {/* <ParticlesBackground /> */}
       </div>
     </>
   );
